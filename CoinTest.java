6d5c4dff9fe1dfa669089873fb23a30833cc6bde
Cody Edwards <edwardscm6@appstate.edu>
2021-11-03 10:50:52 -0400

    Added basic CoinTest, currently always fails

---
+++ CoinTest.java
@@ -0,0 +1,16 @@
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.*;
+
+import java.io.PrintStream;
+import java.io.ByteArrayOutputStream;
+
+public class CoinTest
+{
+    // Needs class to extend Coins and act as constructor for testing
+
+    @Test
+    public void CoinTestTest()
+    {
+        fail("This is supposed to fail");
+    }
+}
