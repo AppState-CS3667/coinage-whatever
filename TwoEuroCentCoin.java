580721c9da1ad1fb897cda2eaa30a794ab8a6a19
Blankenshipgm <blankenshipgm@appstate.edu>
2021-11-04 08:50:40 -0400

    added TwoEruoCentCoin

---
+++ TwoEuroCentCoin.java
@@ -0,0 +1,14 @@
+/**
+ TwoEuroCentCoin
+
+  EUR coin valued at 0.02.
+
+  @author blankenshipgm
+  @version 1
+  */
+public class TwoEuroCentCoin extends AbstractCoin {
+    /** Construct a new coin of this type. */
+    public TwoEuroCentCoin() {
+        super(0.02, "EUR");
+    }
+}
