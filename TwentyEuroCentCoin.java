1097379cfdd2d7122cd4b561bbfa6d173c87c047
Blankenshipgm <blankenshipgm@appstate.edu>
2021-11-04 08:52:54 -0400

    added TwentyEruoCentCoin

---
+++ TwentyEuroCentCoin.java
@@ -0,0 +1,14 @@
+/**
+ Twenty EuroCentCoin
+
+  EUR coin valued at 0.25.
+
+  @author blankenshipgm
+  @version 1
+  */
+public class TwentyEuroCentCoin extends AbstractCoin {
+    /** Construct a new coin of this type. */
+    public TwentyEuroCentCoin() {
+        super(0.25, "EUR");
+    }
+}
