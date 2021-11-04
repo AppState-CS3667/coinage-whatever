85501335848686b2c7f530d89864598a1cb95319
Blankenshipgm <blankenshipgm@appstate.edu>
2021-11-04 08:50:55 -0400

    added FiveTwoEruoCentCoin

---
+++ FiveEuroCentCoin.java
@@ -0,0 +1,14 @@
+/**
+  FiveEuroCentCoin
+
+  EUR coin valued at 0.05.
+
+  @author blankenshipgm
+  @version 1
+  */
+public class FiveEuroCentCoin extends AbstractCoin {
+    /** Construct a new coin of this type. */
+    public FiveEuroCentCoin() {
+        super(0.05, "EUR");
+    }
+}
