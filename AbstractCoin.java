8a578394efddb4c444a7da42716902e396653932
hottellbt <hottellbt@appstate.edu>
2021-11-03 20:09:43 -0400

    documentation

--- AbstractCoin.java
+++ AbstractCoin.java
@@ -1,3 +1,11 @@
+/**
+  AbstractCoin
+
+  The base for all Coin types.
+
+  @author team 'whatever'
+  @version Nov 03
+  */
 public abstract class AbstractCoin {
 
     private double value;
@@ -6,6 +14,12 @@ public abstract class AbstractCoin {
     // int mintingYear;
     // metallic composition later
 
+    /**
+      Creates a new AbstractCoin.
+
+      @param value the value of this coin in the local currency
+      @param countryCode the country code of this coin, e.g. USD
+      */
     public AbstractCoin(double value, String countryCode) {
 
         // guard against "bad" values
@@ -22,10 +36,20 @@ public abstract class AbstractCoin {
         this.countryCode = countryCode;
     }
 
+    /**
+      Returns the value of this coin in the local currency.
+      
+      @return the value
+      */
     public double getValue() {
         return value;
     }
 
+    /**
+      Returns the country code of this coin (never null).
+
+      @return the code
+      */
     public String getCountryCode() {
         return countryCode;
     }
