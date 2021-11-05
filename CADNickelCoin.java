f36ed5110b5c05c2638f2f10c185e643cc5cc762
Cody Edwards <edwardscm6@appstate.edu>
2021-11-05 10:52:48 -0400

    All CAD coins now compile and pass checkstyle

--- CADNickelCoin.java
+++ CADNickelCoin.java
@@ -1,16 +1,15 @@
 /**
-CADNickelCoin
+  CADNickelCoin
 
-CAD coin with value of 0.05
-Nickel name conficts with USD so named CADNickelCoin
+  CAD coin with value of 0.05.
 
-@author edwardscm6
-@version Nov. 5
- */
- public class CADNickelCoin extends AbstractCoin {
-     // Constructor for a new CADNickelCoin type
-     public CADNickelCoin() {
-         // Call to super to create coin
-         super(0.05, "CAD");
-     }
- }
+  @author edwardscm6
+  @version Nov. 5
+  */
+public class CADNickelCoin extends AbstractCoin {
+    /** Constructor for a new CADNickelCoin type. */
+    public CADNickelCoin() {
+        // Call to super to create coin
+        super(0.05, "CAD");
+    }
+}
