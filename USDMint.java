067d756d5d3bf4ef67190242e6d87405c5048da8
hottellbt <hottellbt@appstate.edu>
2021-11-09 18:32:22 -0500

    should return NullCoin.getInstance()

--- USDMint.java
+++ USDMint.java
@@ -43,7 +43,7 @@ public class USDMint extends Mint {
 
         // what the heck???
         // i can't make that!!!
-        return null;
+        return NullCoin.getInstance();
     }
 
     @Override
